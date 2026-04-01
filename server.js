869b5793a8994925acbd793ef4dae271b8754db3
momomint10 <momomint10@gmail.com>
2026-04-01 21:48:20 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 // Supabase 연결
 const supabase = createClient(
